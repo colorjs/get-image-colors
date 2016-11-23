c94f82d8a317edb76ba7dc57dcd90ab59bcc097a
Maxim Geerinck <c4d3r@hotmail.com>
2016-11-23 20:23:38 +0100

    Fix tests

--- test/index.js
+++ test/index.js
@@ -6,44 +6,56 @@ const assert = require('assert')
 describe('get-image-colors', function(){
 
   it('works on JPG images', function(done){
-    getColors(__dirname + '/fixtures/thumb.jpg', function(err, palette){
-      if (err) throw err
+    getColors(__dirname + '/fixtures/thumb.jpg')
+    .then((palette) => {      
       assert(Array.isArray(palette))
       assert(palette.length)
       assert(palette[0].hex().match(/^#[0-9a-f]{3,6}$/i))
       done()
+    })
+    .catch(err => {
+      throw err;
     })
   })
 
   it('works on GIF images', function(done){
-    getColors(__dirname + '/fixtures/thumb.gif', function(err, palette){
-      if (err) throw err
+    getColors(__dirname + '/fixtures/thumb.gif')
+    .then((palette) => {      
       assert(Array.isArray(palette))
       assert(palette.length)
       assert(palette[0].hex().match(/^#[0-9a-f]{3,6}$/i))
       done()
     })
+    .catch(err => {
+      throw err;
+    })
   })
 
   it('works on PNG images', function(done){
-    getColors(__dirname + '/fixtures/thumb.png', function(err, palette){
-      if (err) throw err
+    getColors(__dirname + '/fixtures/thumb.png')
+    .then((palette) => {      
       assert(Array.isArray(palette))
       assert(palette.length)
       assert(palette[0].hex().match(/^#[0-9a-f]{3,6}$/i))
       done()
+    })
+    .catch(err => {
+      throw err;
     })
   })
 
   it('works on SVG images', function(done){
     this.timeout(5000)
-    getColors(__dirname + '/fixtures/thumb.svg', function(err, palette){
-      if (err) throw err
+    getColors(__dirname + '/fixtures/thumb.svg')
+    .then((palette) => {      
       assert(Array.isArray(palette))
       assert(palette.length)
       palette = palette.map(color => color.hex())
       assert(palette[0].match(/^#[0-9a-f]{3,6}$/i))
       done()
+    })
+    .catch(err => {
+      throw err;
     })
   })
 
